07640335aee7561c08c65fa90a1619b64312b075
Hilderlan <hilakt9698@gmail.com>
2020-04-18 18:26:48 -0300

    Models relations and validations added

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :albums
+  
+  validates :name, presence: true
+  has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :favorites
+  has_many :recently_heards
+
+  validates :name, presence: true
 end
