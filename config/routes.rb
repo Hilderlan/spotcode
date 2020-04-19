3212d80e1a552771760b171bc25f0821b0778d40
Hilderlan <hilakt9698@gmail.com>
2020-04-19 14:10:29 -0300

    Endpoint favorites polymorphic finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
 
+  concern :favoritable do |options|
+    shallow do
+      post "/favorite", { to: "favorites#create", on: :member }.merge(options)
+      delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
@@ -10,6 +17,10 @@ Rails.application.routes.draw do
       resources :albums, only: :show do
         resources :recently_heards, only: :create
       end
+      resources :songs, only: [] do
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+      resources :favorites, only: :index
     end
   end
 
