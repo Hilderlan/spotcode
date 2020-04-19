d01facc6aee7a4fcc04a5b54258a49b20f350fc6
Hilderlan <hilakt9698@gmail.com>
2020-04-19 14:05:18 -0300

    Endpoint recentrly_heards finished

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
       resources :dashboard, only: :index
       resources :search, only: :index
       resources :categories, only: %i[index show]
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+      end
     end
   end
 
