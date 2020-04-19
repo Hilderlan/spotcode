9edffb414c442831a472e70685b597c1e533d880
Hilderlan <hilakt9698@gmail.com>
2020-04-18 22:59:22 -0300

    Endpoint search finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :dashboard, only: :index
+      resources :search, only: :index
+      resources :categories, only: :index
     end
   end
 
