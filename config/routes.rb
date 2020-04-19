e56709cb431e6b534c815b4da17db74d861a1fc3
Hilderlan <hilakt9698@gmail.com>
2020-04-18 22:39:32 -0300

    Endpoint discovery finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
+
   root to: 'home#index'
 end
