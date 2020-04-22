6a077fcebd1a9982ed9cdc8ea47c150fa8fa3b1d
Hilderlan <hilakt9698@gmail.com>
2020-04-22 01:23:48 -0300

    Creating the Home Page

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,6 +3,7 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom'
 import Menu from './components/common/menu'
+import './app.scss'
  
 const App = () => {
   return(
--- app/javascript/components/common/menu/index.js
+++ app/javascript/components/common/menu/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment } from 'react';
-import { Navbar, Container, Columns, Button } from 'react-bulma-components';
+import { Navbar, Container, Columns, Button, Dropdown } from 'react-bulma-components';
 import logoImage from '../../assets/images/logo.png'
 import styled from 'styled-components'
+import { useLocation } from 'react-router-dom'
+import { FaUserCircle } from "react-icons/fa";
  
 const ColumnsFullWidth = styled(Columns)`
   width: 100%;
@@ -12,6 +14,28 @@ const ColumnsFullWidth = styled(Columns)`
 `
 
 const Menu = () => {
+  let actionButton;
+ 
+  if (useLocation().pathname == '/') {
+ 	  actionButton = <a href='/users/sign_in' className="is-pulled-right is-right">
+                    <Button outlined={true} color="white">ENTRAR</Button>
+                  </a>
+  }
+  else {
+    actionButton = <Dropdown className='is-pulled-right is-right' color='dark' label={<FaUserCircle size="2em" />}>
+                    <Dropdown.Item value="other">
+                        <a href='/users/edit'>
+                          Edit User
+                        </a>
+                    </Dropdown.Item>
+                    <Dropdown.Item value="other">
+                        <a href='/users/sign_out'>
+                          LogOut
+                        </a>
+                    </Dropdown.Item>
+                  </Dropdown>
+  }
+
   return (
     <Fragment>
       <Navbar color="dark">
@@ -21,9 +45,7 @@ const Menu = () => {
               <img src={logoImage} className='image'/>
             </Columns.Column>
             <Columns.Column>
-              <a href='/users/sign_in' className="is-pulled-right is-right">
-                <Button outlined={true} color="white">ENTRAR</Button>
-              </a>
+              { actionButton }
             </Columns.Column>
           </ColumnsFullWidth>
         </Container>
