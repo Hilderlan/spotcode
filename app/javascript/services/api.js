7f7053c4c6a818f9ff3eb0dc8fe7b496694ae812
Hilderlan <hilakt9698@gmail.com>
2020-04-22 16:06:27 -0300

    Connecting the discovery page with the Backend

--- app/javascript/services/api.js
+++ app/javascript/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const Api = axios.create({baseURL: 'http://localhost:3000'});
+const Api = axios.create({baseURL: 'http://localhost:3000/api/v1'});
 
 export default Api;
