c7e40135f34d5c67f0bc309dd136bb43bcd9310e
Hilderlan <hilakt9698@gmail.com>
2020-04-21 23:26:17 -0300

    Bulma added!

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+import 'app.scss'
  
 const App = () => {
   return(
     <Fragment>
-      Hello
+      <Button color="primary">Testando o Bulma</Button>
     </Fragment>
   );
 }
