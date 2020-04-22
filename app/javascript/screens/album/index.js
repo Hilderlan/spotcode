799f56c12ae7a9b109d24bdf78d1712153af351c
Hilderlan <hilakt9698@gmail.com>
2020-04-22 03:32:01 -0300

    Navigation bar added

--- app/javascript/screens/album/index.js
+++ app/javascript/screens/album/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
         
   const AlbumScreen = () => {
     return(
       <Fragment>
         <div>Album Screen</div>
+        <NavbarFooter/>
       </Fragment>
     );
   }
--- app/javascript/screens/discovery/index.js
+++ app/javascript/screens/discovery/index.js
@@ -1,9 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
         
   const DiscoveryScreen = () => {
     return(
       <Fragment>
         <div>Discovery Screen</div>
+        <NavbarFooter />
       </Fragment>
     );
   }
